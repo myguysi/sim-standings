1b9fee88b42045e9a1f399aeb77073446d42029c
Simon Anthony <sjant90@gmail.com>
2026-05-26 16:42:36 +0100

    Separate build and start scripts

--- src/main.js
+++ src/main.js
@@ -2,17 +2,3 @@ const { LeagueManager } = require('./managers/leagueManager');
 
 const leagueManager = new LeagueManager();
 leagueManager.processRounds();
-
-const express = require('express')
-const app = express()
-const port = 3001
-
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-  res.send('OK')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
